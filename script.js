b4560cdbe80980c77b5ae12257d798069d2a5670
Leonardo Maldonado <lmaldonado@crm.local>
2026-05-08 15:17:33 -0300

    Rolagem das telas de cadastro v1.1

--- script.js
+++ script.js
@@ -438,9 +438,5 @@ app.post('/api/prontuarios', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`🚀 Servidor Mundo Pet rodando em http://localhost:${port}`);
-<<<<<<< HEAD
+    console.log(`🚀 Servidor Mundo Pet rodando em http://localhost:${port}`)
 });
-=======
-});
->>>>>>> ed9ceda72a9bd858ffde6a8dc420c3ae76166e04
